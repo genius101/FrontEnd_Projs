bb98f8b465e9f80113f309295c6c6f2cc362f144
Habeeb Ejio <habeebejio@Habeebs-MacBook-Pro.local>
2022-11-14 10:02:57 +0100

    Almost completed

--- 6and7_proj/script.js
+++ 6and7_proj/script.js
@@ -1,51 +1,13 @@
-// One liner function:
-const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
+const swiper = new Swiper('.swiper', {
+    loop: true,
 
-const imageChange = document.getElementById('change')
-const leftImage = document.getElementById('left')
-const rightImage = document.getElementById('right')
-const image =document.getElementsByClassName('image1')
-var click = false;
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
 
-leftImage.addEventListener('click', ()=> {
-    // imageChange.classList.remove('image1')
-    // Usage: 
-    addCSS(".image1{display:none;}")
-    // addCSS("#change{background:green}")
-    change();
-    click = true;
-    clicked(click);
-    // leftImage.addEventListener('click', ()=> {
-    //     addCSS("#change{background:green}")
-    // }
-    // pic.classList.add('appear')
-    
-})
-rightImage.addEventListener('click', ()=> {
-    // imageChange.classList.remove('image1')
-    // Usage: 
-    // addCSS(".image1{visibility:visible;}")
-    // addCSS(".image1{background-image: url(./img/5.jpg);}")
-})
-
-function change() {
-    var pic = document.createElement("img");
-    // pic.setAttribute("src", "css/img/3.jpg");
-    pic.src ='css/img/3.jpg';
-    document.getElementById("new").appendChild(pic);
-    pic.classList.add('appear');
-}
-
-function clicked(value){
-    if (value == true) {
-        // alert('Picture was clicked')
-        leftImage.addEventListener('click', ()=> {
-            addCSS("#change{background:green}")
-        })
-    }
-}
-
-
-
-
-
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
