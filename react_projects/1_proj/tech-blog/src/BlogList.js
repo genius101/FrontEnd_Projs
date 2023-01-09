27eac5531a48514c37ed704583297a56ade66c0f
Habeeb Ejio <habeebejio@Habeebs-MacBook-Pro.local>
2023-01-09 15:15:20 +0100

    Adding all updates

--- react_projects/1_proj/tech-blog/src/BlogList.js
+++ react_projects/1_proj/tech-blog/src/BlogList.js
@@ -10,6 +10,7 @@ const BlogList = ({ blogs, title}) => {
           <div className="blog-preview" key={blog.id} >
             <h2>{ blog.title }</h2>
             <p>Written by { blog.author }</p>
+            {/* <button onClick={() => handleDelete(blog.id)}>Delete Blog</button> */}
           </div>
         ))}
       </div>
